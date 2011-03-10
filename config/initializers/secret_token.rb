eab2019bf96cc314554cec1d0557015227afc11b
Akash Manohar J <akash@akash.im>
2011-03-10 09:57:30 +0530

    init commit
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Androidcamp::Application.config.secret_token = '304dfaa0ea5edff00f60cb0e6964688db9cf91cb1224d8f64509499d7bac1059d8350e4ca646f6634e65e2006bda5d01abecb497373c2b08ae1b5e214e005a8b'
