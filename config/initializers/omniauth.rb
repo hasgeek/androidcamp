7221071e585746d00d68981f5c3ad428f2c63d9d
Akash Manohar J <akash@akash.im>
2011-03-13 21:47:05 +0530

    removed store setting
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 end
