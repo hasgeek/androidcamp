5c2d9a286788fa9ac453220d53fc699976ec76da
Akash Manohar J <akash@akash.im>
2011-03-13 20:46:08 +0530

    added the gfile again
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+Rails.application.config.middleware.use OmniAuth::Builder do 
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 end
