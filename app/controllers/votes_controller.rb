eb2f6a1f7031bf7aa6b359195f54c9d58a90f535
Akash Manohar J <akash@akash.im>
2011-03-16 01:09:31 +0530

    added conditional block to check for previous vote and added missing relation
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,6 +5,16 @@ class VotesController < ApplicationController
   def create
     @vote = Vote.new(params[:vote])
     @vote.user_id = @current_user.id
+
+    if @vote.valid?
+      votes = @vote.talk.votes.where(:user_id=>@vote.user_id)
+      if votes.count > 0
+        @vote = votes.first
+        render :json => @vote
+        return
+      end
+    end
+
     if request.xhr?
       if @vote.save
         render :json => @vote
@@ -13,4 +23,5 @@ class VotesController < ApplicationController
       end
     end
   end
+
 end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,7 @@
 class Talk < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  has_many :votes
 
   validates_presence_of :title
   validates_presence_of :description
