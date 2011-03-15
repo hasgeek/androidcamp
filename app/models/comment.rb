b905dc130cde003a22a63b7b60a5e4767ff7bffd
Akash Manohar J <akash@akash.im>
2011-03-16 00:13:50 +0530

    completed comments stuff
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :talk
+  belongs_to :user
 
   validates_presence_of :user_id
   validates_presence_of :talk_id
+  validates_presence_of :description, :message=>"Saying nothing doesn't require a comment ;)"
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,35 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+    // talks#show
+    $("#comment_msg").hide();
+    $("#comment_form").bind('ajax:success', function(e, data, status, xhr){
+        var res = jQuery.parseJSON(data);
+        console.log(res);
+        $(".comments").append('<div class="comment">' +
+                              '<div class="author_meta">' +
+                              res['comment']['user_name'] +
+                              '</div>' +
+                              '<div class="description">' +
+                              res['comment']['description'] +
+                              '</div>' +
+                              '</div>');
+    });
+    $("#comment_form").bind('ajax:error', function(e, xhr, status, error){
+        var errorMsg = "akash";
+        console.log("unproc entitry");
+        if(xhr.responseText == "not authorized") {
+            errorMsg = "Please login to comment"
+        }
+        else if(xhr.responseText != undefined) {
+            var res = jQuery.parseJSON(xhr.responseText);
+            errorMsg = res['description'][0];
+        }
+        else {
+            errorMsg = "Oops! something went wrong";
+        }
+        $("#comment_msg").html(errorMsg);
+        $("#comment_msg").show();
+    });
+});
