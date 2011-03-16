353259c6051aaa6383f7b48b6d87c40753d01e24
Akash Manohar J <akash@akash.im>
2011-03-16 22:19:46 +0530

    replaced counter_cache with activerecord callbacks
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,21 @@ class Comment < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :talk_id
   validates_presence_of :description, :message=>"Saying nothing doesn't require a comment ;)"
+
+  before_create :increment_comments_count
+  before_destroy :decrement_comments_count
+  
+  private
+  def increment_comments_count
+    talk = self.talk
+    talk.comments_count = talk.comments_count.succ
+    talk.save
+  end
+
+  def decrement_comments_count
+    talk = self.talk
+    talk.comments_count = talk.comments_count.pred
+    talk.save
+  end
+  
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,4 +4,20 @@ class Vote < ActiveRecord::Base
 
   validates_presence_of :user_id
   validates_presence_of :talk_id
+
+  before_create :increment_votes_count
+  before_destroy :decrement_votes_count
+  
+  private
+  def increment_votes_count
+    talk = self.talk
+    talk.votes_count = talk.votes_count.succ
+    talk.save
+  end
+
+  def decrement_votes_count
+    talk = self.talk
+    talk.votes_count = talk.votes_count.pred
+    talk.save
+  end
 end
